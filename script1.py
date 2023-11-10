e4b420d6c2e01f0dc05a4ee9eb34e1f3d59707a4
Daniel Jordan <daniel.jordan@cx-assoc.com>
2023-11-10 16:48:13 -0500

    building out

--- script1.py
+++ script1.py
@@ -8,9 +8,13 @@ from utils import BrickModel
 g = Graph(load_brick=True)
 g.load_file('models/msl.ttl')
 model = BrickModel(g)
+model.time_frame = ('start', 'end')
 
 # get chiller. I know there's only one chiller so I ask for the first result of the list.
 chiller = model.get_entities(brick_class='Chiller')[0]
+
+# get chiller power meter timeseries
+ts_power_chiller = chiller.get_timeseries(chiller, 'isMeteredBy')
 
 # get pumps
 list_pumps = model.get_entities(brick_class='Pump')
--- utils.py
+++ utils.py
@@ -3,12 +3,39 @@
 """
 
 
+class Entity():
+    """
+
+    """
+    def __init__(self, uri_ref, g):
+        self.uri_ref = uri_ref
+        name = str(uri_ref).split('#')[1]
+        self.name = name
+        self.g = g
+
+    def get_timeseries(self, relationship, inverse_relationship=None):
+        """
+
+        :return:
+        """
+        res = self.g.query(
+            f"""SELECT ?predicate ?object WHERE {{
+                <%s> ?predicate ?object . 
+                     }}
+            """ % self.uri_ref
+        )
+        list_ = res.bindings
+
+        return list_ #ToDo: proof of concept is good ... maybe move this method to something else for the future and
+        # then get back to fetching the timeseries
+
 class BrickModel():
     """
 
     """
     def __init__(self, _graph=None):
         self.g = _graph
+        self.time_frame = (None, None)
 
     def get_entities(self, brick_class=None):
         res = self.g.query(
@@ -17,6 +44,6 @@ class BrickModel():
                      }}"""
         )
         list_ = res.bindings
-        list_friendly = [x.get('sub') for x in list_]
+        list_friendly = [Entity(x.get('sub'), self.g) for x in list_]
 
         return list_friendly
