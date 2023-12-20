20bdd2bff5f5f58d4811cf1f1e47330af8511882
Daniel Jordan <daniel.jordan@cx-assoc.com>
2023-12-20 05:22:04 -0500

    ran another ttl visualization

--- model_visualize.py
+++ model_visualize.py
@@ -9,7 +9,7 @@ from IPython.display import display, Image
 from rdflib.tools.rdf2dot import rdf2dot
 
 # specify filepath
-ttl_filepath = 'models\msl_heating-only.ttl'
+ttl_filepath = "F:/PROJECTS/1297 MMC Bramhall Campus Cx/MBCx EMIS Development/mapping/MMC VAV Boxes.ttl"
 
 # create RDF graph and load data from ttl
 g = rdflib.Graph()
@@ -27,5 +27,5 @@ def visualize(g):
 dg = visualize(g)
 
 png = dg.create_png()
-with open("output_graph3.png", "wb") as f:
+with open("Riaz_MMC_VAV.png", "wb") as f:
     f.write(png)
