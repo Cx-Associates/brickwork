8e04218b7f904d47f5508609bd0bdc2bb15e03fc
riazul.hoque <riazul.hoque@cx-assoc.com>
2024-02-20 08:46:44 -0500

    Additional updates based on model visualization

--- model_visualize.py
+++ model_visualize.py
@@ -9,7 +9,7 @@ from IPython.display import display, Image
 from rdflib.tools.rdf2dot import rdf2dot
 
 # specify filepath
-ttl_filepath = "F:/PROJECTS/1297 MMC Bramhall Campus Cx/MBCx EMIS Development/mapping/MMC VAV Boxes.ttl"
+ttl_filepath = "C:/Users/riazul.hoque/repos/EMIS-brick/subrepos/brickwork/models/msl.ttl"
 
 # create RDF graph and load data from ttl
 g = rdflib.Graph()
@@ -27,5 +27,5 @@ def visualize(g):
 dg = visualize(g)
 
 png = dg.create_png()
-with open("Riaz_MMC_VAV.png", "wb") as f:
+with open("MSL Full Model.png", "wb") as f:
     f.write(png)
